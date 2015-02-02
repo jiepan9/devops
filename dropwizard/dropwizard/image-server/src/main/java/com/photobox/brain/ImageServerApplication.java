14f02cf06f12e6844952d1328db82aa719aad806
Jie <jie.pan@photobox.com>
2015-02-02 15:45:25 +0100

    new files

--- dropwizard/dropwizard/image-server/src/main/java/com/photobox/brain/ImageServerApplication.java
+++ dropwizard/dropwizard/image-server/src/main/java/com/photobox/brain/ImageServerApplication.java
@@ -1,6 +1,8 @@
 package com.photobox.brain;
 
 
+
+import com.codahale.metrics.servlets.MetricsServlet;
 import com.photobox.brain.resource.PhotoResource;
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
@@ -12,10 +14,15 @@ public class ImageServerApplication extends Application<Configuration> {
 
 
     public static void main(String[] args) throws Exception {
+
         new ImageServerApplication().run(args);
     }
     @Override
     public void run(Configuration conf, io.dropwizard.setup.Environment env) throws Exception {
         env.jersey().register(new PhotoResource());
+        env.servlets().addServlet("metrics-servlet",
+            new MetricsServlet(env.metrics())).addMapping("/metrics");
     }
+
+
 }
