b2c641694c1f1ca817dc803cc0c67fd9cb8ca7ab
Jie <jie.pan@photobox.com>
2015-02-02 19:19:56 +0100

    use ImageServerConfiguration class

--- dropwizard/dropwizard/image-server/src/main/java/com/photobox/brain/ImageServerApplication.java
+++ dropwizard/dropwizard/image-server/src/main/java/com/photobox/brain/ImageServerApplication.java
@@ -6,11 +6,12 @@ import com.codahale.metrics.servlets.MetricsServlet;
 import com.photobox.brain.resource.PhotoResource;
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
+import io.dropwizard.setup.Environment;
 
 /**
  * Created by jpan on 15-1-27.
  */
-public class ImageServerApplication extends Application<Configuration> {
+public class ImageServerApplication extends Application<ImageServerConfiguration> {
 
 
     public static void main(String[] args) throws Exception {
@@ -18,7 +19,7 @@ public class ImageServerApplication extends Application<Configuration> {
         new ImageServerApplication().run(args);
     }
     @Override
-    public void run(Configuration conf, io.dropwizard.setup.Environment env) throws Exception {
+    public void run(ImageServerConfiguration conf, io.dropwizard.setup.Environment env) throws Exception {
         env.jersey().register(new PhotoResource());
         //env.servlets().addServlet("metrics-servlet",
            // new MetricsServlet(env.metrics())).addMapping("/metrics");
