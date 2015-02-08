425e512788cf6717af646291c19cd3ebe141bfb1
jie.pan <jie.pan.mail@gmail.com>
2015-02-08 23:17:14 +0100

    add metrics in uri localhost:8081/metrics/

--- vertx/image-server/src/main/java/com/photobox/brain/imageserver/http/ImageServer.java
+++ vertx/image-server/src/main/java/com/photobox/brain/imageserver/http/ImageServer.java
@@ -5,16 +5,16 @@ import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
 import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
 import com.codahale.metrics.jvm.ThreadStatesGaugeSet;
 import org.apache.log4j.BasicConfigurator;
-import org.vertx.java.core.buffer.Buffer;
 import org.vertx.java.core.http.HttpServer;
 import org.vertx.java.core.logging.Logger;
 import org.vertx.java.platform.Verticle;
 import com.codahale.metrics.*;
 import java.io.File;
 import java.lang.management.ManagementFactory;
-import java.util.SortedMap;
+
 import java.util.concurrent.TimeUnit;
 import java.lang.management.RuntimeMXBean;
+
 
 /**
  * Created by jpan on 1/22/15.
@@ -24,9 +24,10 @@ public class ImageServer extends Verticle {
 
     final MetricRegistry metrics = new MetricRegistry();
     final RuntimeMXBean runtime = ManagementFactory.getRuntimeMXBean();
-
+    long start;
 
     public void start(){
+        start = System.currentTimeMillis();
         BasicConfigurator.configure();
         startTestImageServing();
     }
@@ -45,11 +46,14 @@ public class ImageServer extends Verticle {
 
         // counter
         final Meter meter = getMeter();
+        final Timer timer = getTimer();
         HttpServer server = vertx.createHttpServer();
         server.requestHandler(request -> {
              Logger log = container.logger();
              String path = request.path();
              meter.mark();
+
+
              if (path.contains("metrics")) {
                  //metrics
                  StringBuffer sb = new StringBuffer();
@@ -63,19 +67,39 @@ public class ImageServer extends Verticle {
                  sb.append("m1_rate:").append(meter.getOneMinuteRate()).append('\n');
                  sb.append("m5_rate:").append(meter.getFiveMinuteRate()).append('\n');
                  sb.append("mean_rate:").append(meter.getMeanRate()).append('\n');
-                 sb.append("units:").append(meter.)
+
+                 sb.append("Timers:\n");
+                 sb.append("count:").append(timer.getCount()).append('\n');
+                 sb.append("m15rate:").append(timer.getFifteenMinuteRate()).append('\n');
+                 sb.append("m1_rate:").append(timer.getOneMinuteRate()).append('\n');
+                 sb.append("m5_rate:").append(timer.getFiveMinuteRate()).append('\n');
+                 sb.append("mean_rate:").append(timer.getMeanRate()).append('\n');
+
+                 sb.append("snapshot:\n");
+                 sb.append("75th percentile:").append(timer.getSnapshot().get75thPercentile()).append('\n');
+                 sb.append("95th percentile:").append(timer.getSnapshot().get95thPercentile()).append('\n');
+                 sb.append("98th percentile:").append(timer.getSnapshot().get98thPercentile()).append('\n');
+                 sb.append("999th percentile:").append(timer.getSnapshot().get999thPercentile()).append('\n');
+                 sb.append("99th percentile:").append(timer.getSnapshot().get99thPercentile()).append('\n');
+                 sb.append("max:").append(timer.getSnapshot().getMax()).append('\n');
+                 sb.append("mean:").append(timer.getSnapshot().getMean()).append('\n');
+                 sb.append("median:").append(timer.getSnapshot().getMedian()).append('\n');
+                 sb.append("min:").append(timer.getSnapshot().getMin()).append('\n');
+                 sb.append("stdDev:").append(timer.getSnapshot().getStdDev()).append('\n');
 
                  request.response().end(sb.toString());
+                 timer.update(System.currentTimeMillis() - start, TimeUnit.MILLISECONDS);
 
              } else {
                  request.response().sendFile("/space/" + path);
+                 timer.update(System.currentTimeMillis() - start, TimeUnit.MILLISECONDS);
              }
 
 
 
 
 
-            log.info("Count: " + counter.getCount());
+
 
          });
 
@@ -85,9 +109,15 @@ public class ImageServer extends Verticle {
 
     public Meter getMeter() {
 
-        final Meter requests = metrics.meter("requestMeter");
-        return requests;
+        final Meter meter = metrics.meter("requestMeter");
+        return meter;
 
     }
+
+    public Timer getTimer() {
+        final Timer timer = metrics.timer("requestTimer");
+        return timer;
+    }
+
 
 }
